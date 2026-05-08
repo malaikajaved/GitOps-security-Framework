e34a124c0ea215c0f253a7a4c0fdbb05c57548b8
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-08 23:36:45 +0100

    Create insecure S3 bucket in main.tf
    Added an insecure S3 bucket with public access.

--- terraform/insecure/main.tf
+++ terraform/insecure/main.tf
@@ -1,1 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+# INSECURE: S3 bucket with public access and no encryption
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "my-insecure-bucket"
+}
