bf0963600418e95cbde8fe88565116f5da3b69f0
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 22:42:22 +0100

    Create secure IAM policy with minimal permissions
    Added an IAM policy with minimal permissions for S3 actions.

--- terraform/secure/iam.tf
+++ terraform/secure/iam.tf
@@ -1,1 +1,18 @@
+# SECURE: IAM policy with minimal permissions
+resource "aws_iam_policy" "secure_policy" {
+  name = "secure-minimal-policy"
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "arn:aws:s3:::my-secure-bucket/*"
+      }
+    ]
+  })
+}
