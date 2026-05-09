f88ad8788d94432c76540bdbe58cdf9f9f3d7534
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 22:32:29 +0100

    Add insecure S3 bucket and versioning configuration
    Added S3 bucket without versioning and configured versioning as suspended.

--- terraform/insecure/cloudtrail.tf
+++ terraform/insecure/cloudtrail.tf
@@ -1,10 +1,20 @@
 hcl
 # INSECURE: CloudTrail logging disabled
-
 resource "aws_cloudtrail" "insecure_trail" {
   name                          = "insecure-trail"
   s3_bucket_name                = "my-insecure-bucket"
   include_global_service_events = false
   is_multi_region_trail         = false
   enable_logging                = false
+}
+# INSECURE: S3 bucket with no versioning
+resource "aws_s3_bucket" "no_versioning_bucket" {
+  bucket = "my-no-versioning-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "no_versioning" {
+  bucket = aws_s3_bucket.no_versioning_bucket.id
+  versioning_configuration {
+    status = "Suspended"
+  }
 }
