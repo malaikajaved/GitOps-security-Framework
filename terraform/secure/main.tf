7d8742e1316db84b7a4f8ee8a74c8b1a56debe32
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 14:31:11 +0100

    Add server-side encryption configuration for S3 bucket

--- terraform/secure/main.tf
+++ terraform/secure/main.tf
@@ -13,3 +13,6 @@ resource "aws_s3_bucket_acl" "secure_acl" {
   bucket = aws_s3_bucket.secure_bucket.id
   acl    = "private"
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_encryption" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
