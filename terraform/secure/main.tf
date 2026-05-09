1dc6e790c48201fc4be42bc220464167d7713114
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 14:21:38 +0100

    Configure AWS provider and secure S3 bucket
    Add AWS provider and secure S3 bucket configuration

--- terraform/secure/main.tf
+++ terraform/secure/main.tf
@@ -1,1 +1,14 @@
 
+hclprovider "aws" {
+  region = "us-east-1"
+}
+
+# SECURE: S3 bucket with private access and encryption
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "my-secure-bucket"
+}
+
+resource "aws_s3_bucket_acl" "secure_acl" {
+  bucket = aws_s3_bucket.secure_bucket.id
+  acl    = "private"
+}
