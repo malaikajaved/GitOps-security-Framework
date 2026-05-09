e70ad9f5a1e4526c24b7b902a3013c81fb8bc8ba
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 15:41:48 +0100

    Create insecure IAM policy and user
    Added IAM policy with full admin access and created an insecure IAM user without MFA.

--- terraform/insecure/iam.tf
+++ terraform/insecure/iam.tf
@@ -1,1 +1,20 @@
+hcl# INSECURE: IAM policy with full admin access
+resource "aws_iam_policy" "insecure_policy" {
+  name = "insecure-admin-policy"
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# INSECURE: IAM user with no MFA
+resource "aws_iam_user" "insecure_user" {
+  name = "insecure-user"
+}
