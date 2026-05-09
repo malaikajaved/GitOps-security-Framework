95e6a2d3e913600db1d1b63dba94be7ecfa23a10
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 14:06:25 +0100

    Add insecure S3 bucket ACL and security group
    Added public access ACL to S3 bucket and created a security group allowing all inbound traffic.

--- terraform/insecure/main.tf
+++ terraform/insecure/main.tf
@@ -6,3 +6,19 @@ provider "aws" {
 resource "aws_s3_bucket" "insecure_bucket" {
   bucket = "my-insecure-bucket"
 }
+resource "aws_s3_bucket_acl" "insecure_acl" {
+  bucket = aws_s3_bucket.insecure_bucket.id
+  acl    = "public-read"
+}
+
+# INSECURE: Security group allowing all inbound traffic
+resource "aws_security_group" "insecure_sg" {
+  name = "insecure-sg"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
