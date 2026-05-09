3aa6268812d4e86baf488539e01844e410387168
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 22:44:39 +0100

    Add secure CloudTrail resource with logging enabled

--- terraform/secure/cloudtrail.tf
+++ terraform/secure/cloudtrail.tf
@@ -1,1 +1,13 @@
+# SECURE: CloudTrail with logging enabled
+resource "aws_cloudtrail" "secure_trail" {
+  name                          = "secure-trail"
+  s3_bucket_name                = "my-secure-bucket"
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_logging                = true
 
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+}
