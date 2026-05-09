c02f960e1b76117541a45be33932eb3bc2d1f55b
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 14:56:34 +0100

    Update main.tf

--- terraform/secure/main.tf
+++ terraform/secure/main.tf
@@ -22,3 +22,14 @@ rule {
     }
   }
 }
+
+resource "aws_security_group" "secure_sg" {
+  name = "secure-sg"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+}
