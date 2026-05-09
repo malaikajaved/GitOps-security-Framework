9e5a996c2029106b5191d7476fd8bd81f8bcb68e
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-09 22:30:26 +0100

    Create insecure CloudTrail resource
    Added insecure CloudTrail configuration with logging disabled.

--- terraform/insecure/cloudtrail.tf
+++ terraform/insecure/cloudtrail.tf
@@ -1,1 +1,9 @@
-
+hcl
+# INSECURE: CloudTrail logging disabled
+resource "aws_cloudtrail" "insecure_trail" {
+  name                          = "insecure-trail"
+  s3_bucket_name                = "my-insecure-bucket"
+  include_global_service_events = false
+  is_multi_region_trail         = false
+  enable_logging                = false
+}
