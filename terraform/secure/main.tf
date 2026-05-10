4841bdf8dbfe4c88f3efe1e3ef7f5e1d89cc2122
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-10 15:11:25 +0100

    Remove S3 bucket server-side encryption configuration
    Removed server-side encryption configuration for S3 bucket.

--- terraform/secure/main.tf
+++ terraform/secure/main.tf
@@ -1,10 +1,7 @@
-hcl
-
 provider "aws" {
   region = "eu-west-2"
 }
 
-# SECURE: S3 bucket with private access and encryption
 resource "aws_s3_bucket" "secure_bucket" {
   bucket = "my-secure-bucket"
 }
@@ -12,15 +9,8 @@ resource "aws_s3_bucket" "secure_bucket" {
 resource "aws_s3_bucket_acl" "secure_acl" {
   bucket = aws_s3_bucket.secure_bucket.id
   acl    = "private"
-}
-resource "aws_s3_bucket_server_side_encryption_configuration" "secure_encryption" {
-  bucket = aws_s3_bucket.secure_bucket.id
 
-rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
+
 }
 # SECURE: Security group with restricted access
 resource "aws_security_group" "secure_sg" {
