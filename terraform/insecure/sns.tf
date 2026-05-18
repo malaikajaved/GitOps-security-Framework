e572d495df7e4a032845246ea6e37aa42fa41111
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-19 00:02:19 +0100

    Create insecure SNS topic and SQS queue
    Added SNS topic and SQS queue without encryption.

--- terraform/insecure/sns.tf
+++ terraform/insecure/sns.tf
@@ -1,1 +1,9 @@
+# INSECURE: SNS topic without encryption
+resource "aws_sns_topic" "insecure_topic" {
+  name = "insecure-topic"
+}
 
+# INSECURE: SQS queue without encryption
+resource "aws_sqs_queue" "insecure_queue" {
+  name = "insecure-queue"
+}
