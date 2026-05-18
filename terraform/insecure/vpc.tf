7f57d45e0949a57be4b4e62758eab7fe6f3151cc
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-19 00:03:28 +0100

    Document insecure VPC configuration
    Added comments to indicate that this VPC configuration is insecure due to DNS being disabled and no flow logs.

--- terraform/insecure/vpc.tf
+++ terraform/insecure/vpc.tf
@@ -1,1 +1,6 @@
-
+# INSECURE: VPC with DNS disabled and no flow logs
+resource "aws_vpc" "insecure_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = false
+  enable_dns_support   = false
+}
