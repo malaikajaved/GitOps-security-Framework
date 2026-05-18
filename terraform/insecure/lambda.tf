815a4cee62bab51d4f8e337e196d1bc4957eaa17
malaikajaved <147530918+malaikajaved@users.noreply.github.com>
2026-05-19 00:01:10 +0100

    Add warning comment for insecure Lambda function
    Added a warning comment about the insecure nature of the Lambda function.

--- terraform/insecure/lambda.tf
+++ terraform/insecure/lambda.tf
@@ -1,1 +1,8 @@
-
+# INSECURE: Lambda function without encryption or tracing
+resource "aws_lambda_function" "insecure_lambda" {
+  filename      = "lambda.zip"
+  function_name = "insecure-lambda"
+  role          = "arn:aws:iam::123456789012:role/lambda-role"
+  handler       = "index.handler"
+  runtime       = "python3.9"
+}
